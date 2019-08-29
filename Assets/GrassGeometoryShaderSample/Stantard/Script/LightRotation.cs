cec165f111db73e003a62ffb480362d4a83e69fb
y-katsuoka <golden_duck2@hotmail.com>
2019-08-29 21:13:07 +0900

    add await async

---
+++ Assets/GrassGeometoryShaderSample/Stantard/Script/LightRotation.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Threading;
+using System.Threading.Tasks;
+
+public class LightRotation : MonoBehaviour
+{
+    // Start is called before the first frame update
+    async void Start()
+    {
+        await CameraLoop();
+    }
+    
+    private async Task CameraLoop()
+    {
+        float i = 0;
+        while (true)
+        {
+            transform.rotation = Quaternion.Euler(50f, i, 0f);
+
+            await new WaitForEndOfFrame();
+            i += 2f;
+            if (i >= 360) i = 0 ;
+        }
+    }
+
+
+
+}
